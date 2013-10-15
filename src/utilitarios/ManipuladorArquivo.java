e9392958e7c718837ceedcad040a5ec9962f52cf
Maicon Lima <mglima@ciasc.sc.gov.br>
2013-10-15 12:55:40 -0300

    primeiro commut

---
+++ src/utilitarios/ManipuladorArquivo.java
@@ -0,0 +1,39 @@
+package utilitarios;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import javax.swing.JFileChooser;
+
+public class ManipuladorArquivo {
+
+	public ManipuladorArquivo() {}
+	
+	public String lerArquivo(JFileChooser abrirDialog) {
+		String texto = "";
+		try{
+			BufferedReader buffer = new BufferedReader(new FileReader(abrirDialog.getSelectedFile()));
+			String linha;				
+			while ((linha = buffer.readLine()) != null)   {
+				texto = texto + (linha) + "\n";
+			}
+			buffer.close();
+		}catch (Exception e){
+			System.err.println("Erro: " + e.getMessage());
+		}
+		return texto;
+	}
+	
+	public void salvarArquivo(File arquivo, String texto) {		
+		try {
+			BufferedWriter buffer = new BufferedWriter(new FileWriter(arquivo));
+			buffer.write(texto);
+			buffer.close();
+		} catch (Exception e) {
+			System.err.println("Erro: " + e.getMessage());
+		}
+	}
+}
